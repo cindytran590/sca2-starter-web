a104bbbed82a18b94118e1686a6b892df06c8b60
Cindy <ctran33@bostonk12.org>
2019-07-19 16:24:52 -0400

    sca2-starter-web

---
+++ src/containers/About/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <img src="missionstatement.png" alt="Mission Statement" />
+        <img src="crabby.png" alt="Namjoon and his crabs" />
+      </div>
+    )
+  }
+}
+
+export default About
